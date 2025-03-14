76af51a218417e2eab3e0a94ce5843cddefdd254
yeniObabatunde <babatundeabike3@gmail.com>
2025-03-14 23:11:37 +0100

    Create TriviaGameApp.swift

---
+++ TriviaGame/TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Sharon Omoyeni Babatunde on 14/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
